640134f6d3308c7bb6016be30d43b451c438f9ee
Jia Shern <jiashern@hotmail.com>
2015-01-31 17:15:15 +0800

    application.rb updates

--- Desktop/hyve/config/application.rb
+++ Desktop/hyve/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Hyve
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,8 +23,5 @@ module Hyve
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    # config/application.rb
-config.assets.initialize_on_precompile = false
   end
 end
